8c121fb61ef5dc8647eab6094fe85acb9d34e429
Lucas Stefanski <stefaluc000@gmail.com>
2017-04-03 12:37:31 -0500

    Minor

--- src/store.js
+++ src/store.js
@@ -1,15 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
 import thunkMiddleware from 'redux-thunk'
 
-const loggerMiddleware = createLogger()
-
-let middleware = [ thunkMiddleware]
+let middleware = [ thunkMiddleware ]
 // apply logger in dev env
 if (process.env.NODE_ENV !== 'production') {
-  let reduxLogger = require('redux-logger')
-  const loggerMiddleware = reduxLogger.createLogger()
-  middleware = [ ...middlware, loggerMiddleware ]
+  import { createLogger } from 'redux-logger'
+  const loggerMiddleware = createLogger()
+  middleware = [ ...middleware, loggerMiddleware ]
 }
 
 const configureStore = (initialState) => {
